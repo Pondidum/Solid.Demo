a2a59e50f8eb187679b1a4a8fd6de55f25f423d2
andy <andy@andydote.co.uk>
2014-02-19 23:20:03 +0000

    ocp: add removal version

---
+++ Solid.Demo/Ocp/3-Removal/Usage.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+using Solid.Demo.Ocp.Removal.GridHandlers;
+
+namespace Solid.Demo.Ocp.Removal
+{
+	public class Usage : Form
+	{
+		private UserGrid _grid;
+
+		public Usage()
+		{
+			_grid = new UserGrid();
+			_grid.AddHandler(new EmailGridHandler());
+		}
+	}
+}
