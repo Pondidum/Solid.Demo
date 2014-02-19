af5de03ab35eafc1828dee794123c70483f409d2
andy <andy@andydote.co.uk>
2014-02-19 23:16:58 +0000

    ocp: 2: add usage file

---
+++ Solid.Demo/Ocp/2-Implementing/Usage.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+using Solid.Demo.Ocp.Implementing.GridHandlers;
+
+namespace Solid.Demo.Ocp.Implementing
+{
+	public class Usage : Form
+	{
+		private UserGrid _grid;
+
+		public Usage()
+		{
+			_grid = new UserGrid();
+			_grid.AddHandler(new EmailGridHandler());
+		}
+	}
+}
