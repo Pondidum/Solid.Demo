579c912b8bcd3a726988479b7320d105b100162f
andy <andy@andydote.co.uk>
2014-02-22 00:53:09 +0000

    lsp: add mutation example

---
+++ Solid.Demo/Lsp/Mutation/0-Original/IStateGenerator.cs
@@ -0,0 +1,8 @@
+﻿namespace Solid.Demo.Lsp.Mutation.Original
+{
+	public interface IStateGenerator
+	{
+		int GetNextStateID(int currentState);
+		int GetStartStateID();
+	}
+}
---
+++ Solid.Demo/Lsp/Mutation/0-Original/StateMachine.cs
@@ -0,0 +1,41 @@
+﻿namespace Solid.Demo.Lsp.Mutation.Original
+{
+	public class StateMachine
+	{
+		private int _currentState;
+		private readonly IStateGenerator _generator;
+
+		public StateMachine(IStateGenerator generator)
+		{
+			_generator = generator;
+			_currentState = generator.GetStartStateID();
+		}
+
+		public void Transition()
+		{
+			var newState = _generator.GetNextStateID(_currentState);
+
+			switch (newState)
+			{
+				case 0:
+					//do nothing
+					break;
+
+				case 1:
+					//do nothing
+					break;
+
+				case 2:
+					PayTheMan();
+					break;
+			}
+
+			_currentState = newState;
+		}
+
+		private void PayTheMan()
+		{
+			//spend lots of money
+		}
+	}
+}
