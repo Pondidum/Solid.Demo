a168a55443571e80f1db23d9705603def28e38b2
andy <andy@andydote.co.uk>
2014-02-27 22:36:46 +0000

    isp: add an entity class

---
+++ Solid.Demo/Isp/0-Original/Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+namespace Solid.Demo.Isp.Original
+{
+	public class Database
+	{
+		public static void Save(IEntity entity)
+		{
+
+		}
+
+		public static IDataReader Load(Guid id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
