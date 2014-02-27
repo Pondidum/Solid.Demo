9f1b9e998555c4bcafa381705ea27d50faf42729
andy <andy@andydote.co.uk>
2014-02-27 22:38:44 +0000

    isp: added a readonly entity

---
+++ Solid.Demo/Isp/0-Original/ReadOnlyEntity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Solid.Demo.Isp.Original
+{
+	public class ReadOnlyEntity : IEntity
+	{
+		public Guid ID { get; private set; }
+
+		public void Save()
+		{
+			//do nothing
+		}
+
+		public void Load()
+		{
+			using (var reader = Database.Load(ID))
+			{
+				ID = reader.GetGuid(0);
+			}
+		}
+
+		public void ResetChanges()
+		{
+			//do nothing
+		}
+
+		public bool HasChanged()
+		{
+			return false;
+		}
+	}
+}
