31425ed0d23bf21aea54d44e6d3072f65e5b72d9
andy <andy@andydote.co.uk>
2014-03-01 13:32:06 +0000

    isp: add usage

---
+++ Solid.Demo/Isp/2-SeparateInterfaces/Usage.cs
@@ -0,0 +1,11 @@
+﻿namespace Solid.Demo.Isp.SeparateInterfaces
+{
+	public class Usage
+	{
+		public void DoSomething<T>(T entity) where T : ISaveable, ILoadable
+		{
+			entity.Save();
+			entity.Load();
+		} 
+	}
+}
